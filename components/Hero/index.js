4033ae330f3f3c43ed93ca4eda2deeca8193ab7e
Nacho Varni <nachovarni6@gmail.com>
2021-07-01 18:59:41 -0300

    fixing animations

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -28,24 +28,22 @@ function Hero() {
           <h2>Hi there! I am Ignacio Varni, a frontend developer</h2>
         </div>
 
-        {showOptions && (
-          <ul
-            id="menu"
-            className={`${styles.menu} ${
-              showOptions ? styles.menuShow : styles.menuHide
-            }`}
-          >
-            <Link activeClass="active" to="skills" spy={true} smooth={true}>
-              <li className={styles.option}>Skills</li>
-            </Link>
-            <Link activeClass="active" to="work" spy={true} smooth={true}>
-              <li className={styles.option}>Work</li>
-            </Link>
-            <Link activeClass="active" to="contact" spy={true} smooth={true}>
-              <li className={styles.option}>Contact</li>
-            </Link>
-          </ul>
-        )}
+        <ul
+          id="menu"
+          className={`${styles.menu} ${
+            showOptions ? styles.menuShow : styles.menuHide
+          }`}
+        >
+          <Link activeClass="active" to="skills" spy={true} smooth={true}>
+            <li className={styles.option}>Skills</li>
+          </Link>
+          <Link activeClass="active" to="work" spy={true} smooth={true}>
+            <li className={styles.option}>Work</li>
+          </Link>
+          <Link activeClass="active" to="contact" spy={true} smooth={true}>
+            <li className={styles.option}>Contact</li>
+          </Link>
+        </ul>
       </div>
       <div className={styles.info}>
         <Skills />
